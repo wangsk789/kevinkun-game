44539108a5aa69dbd3105a948937f159c5cc0ee3
wangsk789 <wangsk789@163.com>
2020-02-03 20:27:30 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,101 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var port = process.env.PORT || 5050;
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
+
+
+var matchlist = [];
+var maxUserNum = 2;
+var roomindex = 0;
+
+io.on('connection', function (socket) {
+
+
+	socket.on("login",function (username){
+
+			socket.username=username;
+			var data ={};
+			socket.emit("logedin",data);
+			console.log(socket.username +"登录服务器");
+
+	});
+	socket.on("match",function(){
+		var index = matchlist.indexOf(socket);
+		if(index == -1){
+			matchlist.push(socket);
+			if(matchlist.length>=maxUserNum){
+				roomindex++;
+				var roomname="room" + roomindex;
+				var userlist = [];
+				
+				for(var i=0; i < maxUserNum; i++){
+					matchlist[i].join(roomname);
+					matchlist[i].roomname=roomname;
+					userlist.push(matchlist[i].username);
+					
+				}
+				matchlist.splice(0,2);			
+				io.sockets.in(roomname).emit('matched', {"userlist":userlist}); 
+				console.log("新房间：" +roomname);
+			}
+			console.log("当前排队人数"+matchlist.length);
+		}
+		
+	});
+	
+	socket.on("cancelmatch",function(){
+		var index = matchlist.indexOf(socket);
+		if(index !== -1){
+			matchlist.splice(index,1);
+			console.log(socket.username + "退出排队",);
+		}
+		
+	});
+	
+
+	
+	socket.on('newEvent', function (eventName, eventContent) {
+		if(socket.roomname){
+			var data ={};
+			data.from = socket.username;
+			data.name = eventName;
+			data.content = eventContent;
+			io.sockets.in(socket.roomname).emit('gotEvent', data); 
+			console.log(socket.username +"在房间"+socket.roomname +"中发送事件："+ eventName + "，事件内容：" +eventContent);
+		}
+			
+
+	});
+  
+  
+	socket.on('quitGame', function () {
+		if(socket.roomname){
+			socket.leave(socket.roomname);
+			io.sockets.in(socket.roomname).emit('otherLeft', {"leftuser":socket.username}); 
+			socket.roomname="";
+		}
+	});
+  
+
+
+	socket.on('disconnect', function () {
+		if(socket.roomname){
+			socket.leave(socket.roomname);
+			io.sockets.in(socket.roomname).emit('otherLeft', {"leftuser":socket.username}); 
+			socket.roomname="";
+		}
+
+   
+	});
+
+
+
+
+});
+http.listen(port, function(){
+  console.log('listening on *:' + port);
+});
